0f5b9043c5f4d86cf94bcb91fa75e0b66846697f
jfm92 <jfloresm92@gmail.com>
2020-09-15 12:10:08 +0200

    Changing localitation of LVGL library & updating to v7.4.0

--- components/GUI/component.mk
+++ components/GUI/component.mk
@@ -1,2 +1,18 @@
-COMPONENT_ADD_INCLUDEDIRS := .
-COMPONENT_SRCDIRS := .
+#COMPONENT_ADD_INCLUDEDIRS := .
+#COMPONENT_SRCDIRS := .
+
+ifndef LV_CONF_INCLUDE_SIMPLE
+CFLAGS += -DLV_CONF_INCLUDE_SIMPLE
+endif
+
+COMPONENT_SRCDIRS := LVGL/ \
+	LVGL/src/lv_core \
+	LVGL/src/lv_draw \
+	LVGL/src/lv_widgets \
+	LVGL/src/lv_hal \
+	LVGL/src/lv_misc \
+	LVGL/src/lv_themes \
+	LVGL/src/lv_font \
+	.
+
+COMPONENT_ADD_INCLUDEDIRS := $(COMPONENT_SRCDIRS) .
