e30231f7774cfa7c76177e8bd584392f685fff2f
jfm92 <jfloresm92@gmail.com>
2020-09-21 20:11:06 +0200

    Updating pin configuration

--- components/drivers/system_configuration/system_configuration.h
+++ components/drivers/system_configuration/system_configuration.h
@@ -1,48 +1,36 @@
 
 /********************************
- *      Screen Configuration
+ *    Display pin configuration
  * ******************************/
 
 #define SCR_MODEL       ST7789
 #define SCR_WIDTH       240
 #define SCR_HEIGHT      240
-#define SCR_BACKLIGTH   FALSE
 
 #define HSPI_MOSI        13
 #define HSPI_CLK         14
 #define HSPI_RST         33
-#define HSPI_DC          27
-#define HSPI_BACKLIGTH   -1
+#define HSPI_DC          32
+#define HSPI_BACKLIGTH   15
 #define HSPI_CLK_SPEED   40*1000*1000
 
-/********************************
- *      Button Configuration
- * ******************************/
-
-#define BTN_UP          34
-#define BTN_DOWN        35
-#define BTN_RIGHT       25
-#define BTN_LEFT        32
-#define BTN_A           26
-#define BTN_B           12
-#define BTN_START       2
-#define BTN_SELECT      15
 
 /********************************
- *   Flash memory Configuration
+ *   SD CARD pin configuration
  * ******************************/
-//TODO: Add all the GPIO
+
 #define VSPI_MOSI       23 
 #define VSPI_MISO       19 
 #define VSPI_CLK        18        
 #define VSPI_CS0        5                  
-#define VSPI_CLK_SPEED  40*1000*1000
-#define FLASH_SIZE      128     //128 MBytes
+#define VSPI_CLK_SPEED  18*1000*1000
 
 /********************************
- *      System Features
+ *   Pin Mux pin configuration
  * ******************************/
 
-#define SOUND_EN        FALSE
-#define INIT_MENU_EN    TRUE
-#define SD_CARD_EN      FALSE
+#define MUX_SDA         21
+#define MUX_SCL         22
+#define MUX_INT         34
+#define I2C_CLK         400*1000
+#define I2C_dev_address 0x20
