8f28639956bf505abcb45d7739c4afca1fb8fe8d
jfm92 <jfloresm92@gmail.com>
2020-10-01 18:37:56 +0200

    Added NES to the display HAL

--- components/drivers/display_hal/display_hal.c
+++ components/drivers/display_hal/display_hal.c
@@ -11,16 +11,18 @@
 #include "st7789.h"
 #include "system_configuration.h"
 
-uint16_t* line[LINE_BUFFERS];
+uint16_t *line[LINE_BUFFERS];
+extern uint16_t myPalette[];
 
-static uint16_t getPixel(const uint16_t *bufs, uint16_t x, uint16_t y, uint16_t w2, uint16_t h2);
+static uint16_t getPixelGBC(const uint16_t *bufs, uint16_t x, uint16_t y, uint16_t w2, uint16_t h2);
+static uint8_t getPixelNES(const uint8_t *bufs, uint16_t x, uint16_t y, uint16_t w2, uint16_t h2);
 
 void display_init(void){
 
     // Create display buffer
     const size_t lineSize = SCR_WIDTH * LINE_COUNT * sizeof(uint16_t);
     for (int x = 0; x < LINE_BUFFERS; x++){
-        line[x] = heap_caps_malloc(lineSize, MALLOC_CAP_DMA | MALLOC_CAP_8BIT);
+        line[x] = heap_caps_malloc(lineSize,  MALLOC_CAP_8BIT);
         if (!line[x]) abort();
     }
 
@@ -83,7 +85,7 @@ void display_gb_frame(const uint16_t *data){
                 uint16_t index = (i)*outWidth;
 
                 for(uint16_t x = 0; x < outWidth; x++){
-                    uint16_t sample = getPixel(data, x, (y + i), outWidth, outHeight);
+                    uint16_t sample = getPixelGBC(data, x, (y + i), outWidth, outHeight);
                     
                     // Save the calculate pixel to the line buffer
                     index ++;
@@ -96,12 +98,112 @@ void display_gb_frame(const uint16_t *data){
             calc_line = (calc_line == 1) ? 0 : 1;
             scr_spi_send_lines(y, 0, outWidth, line[sending_line], LINE_COUNT);
         }
+      /*  short outputHeight = SCR_HEIGHT;
+        short   outputWidth = GB_FRAME_WIDTH + (SCR_HEIGHT - GB_FRAME_HEIGHT);
+        short   xpos = (SCR_WIDTH - outputWidth) / 2;
 
+            for (int y = 0; y < outputHeight; y += LINE_COUNT)
+            {
+                for (int i = 0; i < LINE_COUNT; ++i)
+                {
+                    if ((y + i) >= outputHeight)
+                        break;
+
+                    int index = (i)*outputWidth;
+
+                    for (int x = 0; x < outputWidth; ++x)
+                    {
+                        uint16_t sample = getPixel(data, x, (y + i), outputWidth, outputHeight);
+                        line[calc_line][index++] = ((sample >> 8) | ((sample) << 8));
+                    }
+                }
+               
+                sending_line = calc_line;
+                calc_line = (calc_line == 1) ? 0 : 1;
+                scr_spi_send_lines(y, xpos, outputWidth, line[sending_line], LINE_COUNT);
+            }*/
+            
     }
 }
 
-// REWORK
-static uint16_t getPixel(const uint16_t *bufs, uint16_t x, uint16_t y, uint16_t w2, uint16_t h2){
+void display_NES_frame(const uint8_t *data){
+    uint16_t calc_line = 0;
+    uint16_t sending_line = 0;
+
+    if(data == NULL){
+        for(uint16_t y = 0; y < SCR_HEIGHT; y++){
+
+            for(uint16_t x = 0; x < SCR_WIDTH; x++){
+                line[calc_line][x] = 0; 
+            }
+            
+            sending_line = calc_line;
+            calc_line = (calc_line == 1) ? 0 : 1;
+            scr_spi_send_lines(y, 0, SCR_WIDTH, line[sending_line], 1);
+        }
+    }
+    else{
+        int outputHeight = 240;
+        int    outputWidth = 240 + (240 - 240);
+        int xpos = (240 - outputWidth) / 2;
+
+        for (int y = 0; y < outputHeight; y += LINE_COUNT){
+            for (int i = 0; i < LINE_COUNT; ++i){
+                if ((y + i) >= outputHeight)
+                break;
+
+                int index = (i)*outputWidth;
+
+                for (int x = 0; x < outputWidth; x++){
+                    line[calc_line][index++] = myPalette[getPixelNES(data, x, (y + i), outputWidth, outputHeight)];
+                }
+            }
+
+            sending_line = calc_line;
+            calc_line = (calc_line == 1) ? 0 : 1;
+            scr_spi_send_lines(y, xpos, outputWidth, line[sending_line], LINE_COUNT);
+        }
+    }
+
+
+}
+
+
+static uint8_t getPixelNES(const uint8_t *bufs, uint16_t x, uint16_t y, uint16_t w2, uint16_t h2){
+
+    int x_diff, y_diff, xv, yv, red, green, blue, col, a, b, c, d, index;
+    int x_ratio = (int)(((NES_FRAME_WIDTH - 1) << 16) / w2) + 1;
+    int y_ratio = (int)(((NES_FRAME_HEIGHT - 1) << 16) / h2) + 1;
+
+    xv = (int)((x_ratio * x) >> 16);
+    yv = (int)((y_ratio * y) >> 16);
+
+    x_diff = ((x_ratio * x) >> 16) - (xv);
+    y_diff = ((y_ratio * y) >> 16) - (yv);
+
+    index = yv * NES_FRAME_WIDTH + xv;
+
+    a = bufs[index];
+    b = bufs[index + 1];
+    c = bufs[index + NES_FRAME_WIDTH];
+    d = bufs[index + NES_FRAME_WIDTH + 1];
+
+    red = (((a >> 11) & 0x1f) * (1 - x_diff) * (1 - y_diff) + ((b >> 11) & 0x1f) * (x_diff) * (1 - y_diff) +
+           ((c >> 11) & 0x1f) * (y_diff) * (1 - x_diff) + ((d >> 11) & 0x1f) * (x_diff * y_diff));
+
+    green = (((a >> 5) & 0x3f) * (1 - x_diff) * (1 - y_diff) + ((b >> 5) & 0x3f) * (x_diff) * (1 - y_diff) +
+             ((c >> 5) & 0x3f) * (y_diff) * (1 - x_diff) + ((d >> 5) & 0x3f) * (x_diff * y_diff));
+
+    blue = (((a)&0x1f) * (1 - x_diff) * (1 - y_diff) + ((b)&0x1f) * (x_diff) * (1 - y_diff) +
+            ((c)&0x1f) * (y_diff) * (1 - x_diff) + ((d)&0x1f) * (x_diff * y_diff));
+
+    col = ((int)red << 11) | ((int)green << 5) | ((int)blue);
+
+    return col;
+
+}
+static uint16_t getPixelGBC(const uint16_t *bufs, uint16_t x, uint16_t y, uint16_t w2, uint16_t h2){
+
     int x_diff, y_diff, xv, yv, red, green, blue, col, a, b, c, d, index;
     int x_ratio = (int)(((GB_FRAME_WIDTH - 1) << 16) / w2) + 1;
     int y_ratio = (int)(((GB_FRAME_HEIGHT - 1) << 16) / h2) + 1;
@@ -132,3 +234,5 @@ static uint16_t getPixel(const uint16_t *bufs, uint16_t x, uint16_t y, uint16_t 
 
     return col;
 }
+
+
--- components/drivers/display_hal/display_hal.h
+++ components/drivers/display_hal/display_hal.h
@@ -17,8 +17,11 @@ extern "C"
 #define LINE_BUFFERS (2)
 #define LINE_COUNT   (4)
 
-#define GB_FRAME_WIDTH 160
-#define GB_FRAME_HEIGHT 144
+#define GB_FRAME_WIDTH  160 
+#define GB_FRAME_HEIGHT 144 
+
+#define NES_FRAME_WIDTH 256
+#define NES_FRAME_HEIGHT 224
 
 /**********************
  * GLOBAL PROTOTYPES
@@ -27,6 +30,7 @@ extern "C"
 void display_init(void);
 void display_clear(void);
 void display_gb_frame(const uint16_t *data);
+void display_NES_frame(const uint8_t *data);
 void display_flush(lv_disp_drv_t * drv, const lv_area_t * area, lv_color_t * color_map);
 
 #ifdef __cplusplus
