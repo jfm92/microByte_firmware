365c0152c5ca3802a7986355b7ec8a670544507b
jfm92 <jfloresm92@gmail.com>
2020-10-07 11:43:50 +0200

    Modife to make it compatible with the new emulator management

--- components/emulators/NES/nofrendo/nofrendo.h
+++ components/emulators/NES/nofrendo/nofrendo.h
@@ -41,15 +41,6 @@ int nofrendo_main(int argc, char *argv[]);
 
 extern volatile int nofrendo_ticks; /* system timer ticks */
 
-/* osd_main should end with a call to main_loop().
-** Pass filename = NULL if you want to start with the demo rom 
-*/
-extern int main_loop(const char *filename, system_t type);
-
-/* These should not be called directly. Use the event interface */
-extern void main_insert(const char *filename, system_t type);
-extern void main_eject(void);
-extern void main_quit(void);
 
 #endif /* !_NOFRENDO_H_ */
 
