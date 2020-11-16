6655523522df71b82e463f20a1641990c4d15114
jfm92 <jfloresm92@gmail.com>
2020-11-16 14:08:42 +0100

    Added support to save the files on the flash memory and improved the commentary to a better understanding of the functions

--- components/drivers/sd_storage/sd_storage.c
+++ components/drivers/sd_storage/sd_storage.c
@@ -48,7 +48,7 @@ static const char *TAG = "SD_CARD";
  *   GLOBAL FUNCTIONS
  **********************/
 
-uint8_t sd_init(){
+bool sd_init(){
     ESP_LOGI(TAG, "Init SD Card");
 
     sdmmc_host_t host = SDSPI_HOST_DEFAULT();
@@ -61,7 +61,7 @@ uint8_t sd_init(){
     host.slot               =     VSPI_HOST;
     host.max_freq_khz       =     SDMMC_FREQ_DEFAULT;
 
-    // Mount Fat filesystem, it only can open one file at the same time
+    // Mount Fat filesystem(Only one file at the time)
     esp_vfs_fat_sdmmc_mount_config_t mount_config = {
         .format_if_mount_failed = false,
         .max_files = 1,
@@ -77,7 +77,7 @@ uint8_t sd_init(){
         } else {
             ESP_LOGE(TAG, "Failed to initialize the card.");
         }
-        return -1;
+        return false;
     }
 
     // Check if the folder tree exists. If not create it.
@@ -107,6 +107,15 @@ uint8_t sd_init(){
         mkdir("/sdcard/Master_System", 0700);
         mkdir("/sdcard/Master_System/Save_Data", 0700);
     }
+    if(stat("/sdcard/Game_Gear", &st) == -1){
+        ESP_LOGI(TAG, "No Game_Gear folder found, creating it");
+        mkdir("/sdcard/Game_Gear", 0700);
+        mkdir("/sdcard/Game_Gear/Save_Data", 0700);
+    }
+    if(stat("/sdcard/apps", &st) == -1){
+        ESP_LOGI(TAG, "No apps folder found, creating it");
+        mkdir("/sdcard/apps", 0700);
+    }
 
     SD_mount = true;
 
@@ -126,33 +135,37 @@ uint8_t sd_init(){
     ESP_LOGI(TAG,"SD Card detected:\n -Name: %s\n -Capacity: %i MB\n -Speed: %i Khz\n -Type: %i\n" \
     ,sd_card_info.card_name,sd_card_info.card_size,sd_card_info.card_speed,sd_card_info.card_type);
 
-    return 1;
+    return true;
 }
 
 
 uint8_t sd_game_list(char game_name[30][100],uint8_t console){
     
     struct dirent *entry;
-
+    //TODO: Improve game name management, at this moment the max size of the name is 100 characters and 30 games per console
+    // Open the folder of the specific console
     DIR *dir = NULL;
     if(console == NES) dir =  opendir("/sdcard/NES");
     else if(console == GAMEBOY) dir =  opendir("/sdcard/GameBoy");
     else if(console == GAMEBOY_COLOR) dir =  opendir("/sdcard/GameBoy_Color");
     else if(console == SNES) dir =  opendir("/sdcard/SNES");
     else if(console == SMS) dir =  opendir("/sdcard/Master_System");
+    else if(console == GG) dir =  opendir("/sdcard/Game_Gear");
 
 
     if (!dir) {
         ESP_LOGE(TAG, "Failed to stat dir : 0x%02x", console);
         return 0;
     }
-    // Save the name of all the files available on the external flash
+    
+    //Loop to find the game of each console base on the file extension
     uint8_t i =0;
     while((entry = readdir(dir)) != NULL){
  
         size_t nameLength = strlen(entry->d_name);
-        // TODO: Rework game list maker.
-        // Only adds a game with the console termination.
+
+        // TODO: Rework game list maker, set by alphabetical order
+
         if ((strcmp(entry->d_name + (nameLength - 4), ".nes") == 0) && console == NES) {
             sprintf(game_name[i],"%s",entry->d_name);
             ESP_LOGI(TAG, "Found %s ",game_name[i]);
@@ -173,10 +186,39 @@ uint8_t sd_game_list(char game_name[30][100],uint8_t console){
             ESP_LOGI(TAG, "Found %s ",game_name[i]);
             i++;
         }
+        else if ((strcmp(entry->d_name + (nameLength - 3), ".gg") == 0) && console == GG){
+            sprintf(game_name[i],"%s",entry->d_name);
+            ESP_LOGI(TAG, "Found %s ",game_name[i]);
+            i++;
+        }
     }
     // Return the number of files
     return i;
 }
+
+uint8_t sd_app_list(char app_name[30][100],bool update){
+    struct dirent *entry;
+
+    DIR *dir = NULL;
+    if(update) dir =  opendir("/sdcard");
+    else dir =  opendir("/sdcard/apps");
+    
+    // Only find .bin file on the apps folder
+    uint8_t i =0;
+    while((entry = readdir(dir)) != NULL){
+        
+        size_t nameLength = strlen(entry->d_name);
+
+        if(strcmp(entry->d_name + (nameLength - 4), ".bin") == 0) {
+            sprintf(app_name[i],"%s",entry->d_name);
+            ESP_LOGI(TAG, "Found %s ",app_name[i]);
+            i++;
+        }
+    }
+
+    return i;
+}
+    
 
 size_t sd_file_size(const char *path){
     
@@ -186,12 +228,11 @@ size_t sd_file_size(const char *path){
     size_t actual_size = ftell(fd);
     fseek(fd, 0, SEEK_SET);
 
-    ESP_LOGI(TAG,"Game size: %i bytes",actual_size);
+    ESP_LOGI(TAG,"Size: %i bytes",actual_size);
     fclose(fd);
 
     return actual_size;
 }
-
 
 void sd_get_file (const char *path, void * data){
     const size_t BLOCK_SIZE = 512;// We're going to read file in chunk of 512 Bytes
@@ -211,10 +252,58 @@ void sd_get_file (const char *path, void * data){
         if (count < BLOCK_SIZE) break;
     }
 
+    fclose(fd);
+}
+
+
+char * IRAM_ATTR sd_get_file_flash (const char *path){
+     const size_t BLOCK_SIZE = 4096*2;
+    char *map_ptr;// Pointer to file in the internal flash.
+    
+    spi_flash_mmap_handle_t map_handle;
+
+    const esp_partition_t *partition = esp_partition_find_first(ESP_PARTITION_TYPE_DATA, ESP_PARTITION_SUBTYPE_ANY, "data_0");
+    if(partition == NULL){
+        ESP_LOGE(TAG, "Partition NULL");
+    }
+    ESP_LOGI(TAG,"Partition label %s, ffset 0x%x with size 0x%x\r\n",partition->label,partition->address, partition->size);
+    
+
+    ESP_ERROR_CHECK(esp_partition_erase_range(partition, 0, partition->size));
+
+    size_t r = 0;
+
+    FILE *fd = fopen(path, "rb"); //Open the file in binary read mode
+
+    if(fd==NULL){
+       ESP_LOGE(TAG, "Error opening: %s ",path);
+    }
+
+    char * temp_buffer;
+    temp_buffer =  malloc(BLOCK_SIZE);
+    esp_fill_random(temp_buffer,BLOCK_SIZE);
+
+    while (true){
+        __asm__("memw"); // Protect the write into the RAM memory
+        size_t count = fread(temp_buffer, 1, BLOCK_SIZE, fd);
+        esp_partition_write(partition, r, temp_buffer, BLOCK_SIZE);
+        __asm__("memw");
+
+        r += count;
+        if (count < BLOCK_SIZE) break;
+    }
+
+    close(fd);
+    // Return a pointer to the position of the saved file on the internal flash.
+
+	ESP_ERROR_CHECK(esp_partition_mmap(partition, 0, partition->size, SPI_FLASH_MMAP_DATA, &map_ptr, &map_handle));
+   return map_ptr;
 }
 
 
 bool sd_mounted(){
     return SD_mount;
 }
+
+
 
--- components/drivers/sd_storage/sd_storage.h
+++ components/drivers/sd_storage/sd_storage.h
@@ -1,3 +1,6 @@
+/*********************
+ *      INCLUDES
+ *********************/
 #include "stdint.h"
 #include "stdbool.h"
 
@@ -6,6 +9,9 @@
 #define SDHC    0x02
 #define SDSC    0x03
 
+/*********************
+ *      DEFINES
+ *********************/
 struct sd_card_info{
     char card_name[32];
     uint8_t card_type;
@@ -13,13 +19,118 @@ struct sd_card_info{
     uint32_t card_speed;
 };
 
+/*********************
+ *      EXTERNS
+ *********************/
 extern struct sd_card_info sd_card_info;
 
-uint8_t sd_init();
+/*********************
+ *      FUNCTIONS
+ *********************/
+
+/*
+ * Function:  sd_init 
+ * --------------------
+ * 
+ * Initialize the SPI bus and set the proper configuration to set-up the FAT file system.
+ * 
+ * Returns: True if the initialization suceed otherwise false.
+ * 
+ */
+bool sd_init();
+
+
+/*
+ * Function:  sd_game_list 
+ * --------------------
+ * 
+ * This function  gives the number of game available and the name of them for each console.
+ * 
+ * Arguments:
+ *  -game_name: Two dimensions array in which will be save the game names.
+ *  -console: Console to check the available games.
+ * 
+ * Returns: Number of available games for the console.
+ * 
+ */
+uint8_t sd_game_list(char game_name[30][100], uint8_t console);
+
+/*
+ * Function:  sd_app_list 
+ * --------------------
+ * 
+ * This function gives the number of update or external applications available on the SD card.
+ * 
+ * Arguments:
+ *  -app_name: Two dimensions array in which will be save the external application name or the updates available on the SD
+ *  -update: True is you are looking for an update binary, otherwise false for the external applications.
+ * 
+ * Returns: Number of available external apps of updates binaries.
+ * 
+ */
+uint8_t sd_app_list(char app_name[30][100], bool update);
+
+/*
+ * Function:  sd_file_size 
+ * --------------------
+ * 
+ * Given a file path, it returns the size of the file
+ * 
+ * Arguments:
+ *  -path: Valid path to the file.
+ * 
+ * Returns: The size of the file in bytes.
+ * 
+ */
+size_t sd_file_size(const char *path);
+
+/*
+ * Function:  sd_get_file 
+ * --------------------
+ * 
+ * Given a valid file path, copy a file from the SD card to the RAM memory.
+ * 
+ * Note: Only valid for files with a size under 3 MBytes.
+ * 
+ * Arguments:
+ *  -path: Valid path to the file.
+ *  -data: Pointer to the memory region where the data will be saved.
+ * 
+ * Returns: Nothing.
+ * 
+ */
+void  sd_get_file (const char *path, void * data);
+
+/*
+ * Function:  sd_get_file_flash 
+ * --------------------
+ * 
+ * Given a valid file path, copy a file from the SD card to the "storage" partition on the flash memory.
+ * 
+ * Note: A valid 4 Mbyte data partition should set previously on the partition.csv file to use this feature.
+ * It's not recommendable to use in files under 3 MByte, because the write speed is significantly slower than
+ * the memory RAM alternative.
+ * 
+ * Arguments:
+ *  -path: Valid path to the file.
+ * 
+ * Returns: Pointer to the region of the flash memory where is allocated the data.
+ * 
+ */
+char * IRAM_ATTR sd_get_file_flash (const char *path);
+
+/*
+ * Function:  sd_mounted 
+ * --------------------
+ * 
+ * Return if the SD Card and the filesystem was properly mounted.
+ * 
+ * Returns: True if it was mounted the SD Card
+ * 
+ */
+bool sd_mounted();
+
+/*********WIP**********/
 void sd_format();
 void sd_unmount();
 void sd_mount();
-uint8_t sd_game_list(char game_name[30][100], uint8_t console);
-size_t sd_file_size(const char *path);
-void  sd_get_file (const char *path, void * data);
-bool sd_mounted();
