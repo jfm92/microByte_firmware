4c3de2336c0c68c7d2d06bfb1b4d3b55d9c0ed08
jfm92 <jfloresm92@gmail.com>
2020-11-20 11:03:14 +0100

    Clean up and adding comments to audio driver

--- components/drivers/sound/sound_driver.c
+++ components/drivers/sound/sound_driver.c
@@ -1,18 +1,37 @@
-#include "sound_driver.h"
+/*********************
+ *      INCLUDES
+ *********************/
 
 #include "freertos/FreeRTOS.h"
+
+#include "esp_err.h"
+#include "esp_log.h"
+
 #include "driver/i2s.h"
 #include "driver/rtc_io.h"
 
-// TODO: Save the volume level on the static memory
+#include "sound_driver.h"
+#include "system_configuration.h"
 
-static float volume_level = 0.8f; // 80% of the sound level
+/**********************
+*      VARIABLES
+**********************/
+float volume_level = 0.8f; // When starts the sound level it's at the 80%
 
-void audio_init(int sample_rate)
-{
-    printf("%s: sample_rate=%d\n", __func__, sample_rate);
+/**********************
+*  STATIC VARIABLES
+**********************/
+static const char *TAG = "SOUND_DRIVER";
 
-    // NOTE: buffer needs to be adjusted per AUDIO_SAMPLE_RATE
+/**********************
+ *   GLOBAL FUNCTIONS
+ **********************/
+
+bool audio_init(uint32_t sample_rate){
+    ESP_LOGI(TAG,"Audio configuration init.");
+
+    ESP_LOGI(TAG,"Audio Sample Rate: %i",sample_rate);
+
     i2s_config_t i2s_config = {
         .mode = I2S_MODE_MASTER | I2S_MODE_TX, // Only TX
         .sample_rate = sample_rate,
@@ -20,59 +39,65 @@ void audio_init(int sample_rate)
         .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT, //2-channels
         .communication_format = I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB,
         .dma_buf_count = 6,
-        .dma_buf_len = 512,                       // (416samples * 2ch * 2(short)) = 1664
-        .intr_alloc_flags = 0, //Interrupt level 1
+        .dma_buf_len = 512,
+        .intr_alloc_flags = 0, 
         .use_apll = false};
 
-    i2s_driver_install(I2S_NUM, &i2s_config, 0, NULL);
+    if(i2s_driver_install(I2S_NUM, &i2s_config, 0, NULL) != ESP_OK){
+        ESP_LOGE(TAG,"I2S driver error install error");
+        return false;
+    }
+
     i2s_pin_config_t pin_config = {
-        .bck_io_num = 27,
-        .ws_io_num = 26,
-        .data_out_num = 25,
+        .bck_io_num = I2S_BCK,
+        .ws_io_num = I2S_WS,
+        .data_out_num = I2S_DATA_O,
         .data_in_num = -1 //Not used
     };
-    i2s_set_pin(I2S_NUM, &pin_config);
+
+    if(i2s_set_pin(I2S_NUM, &pin_config) != ESP_OK){
+        ESP_LOGE(TAG,"I2S set pin error.");
+        return false;
+    }
+
+    return true;
 }
 
-void audio_submit(short *stereoAudioBuffer, int frameCount)
-{
+void audio_submit(short *stereoAudioBuffer, uint32_t frameCount){
 
-    short currentAudioSampleCount = frameCount * 2;
+    uint32_t audio_length = frameCount * 2 * sizeof(int16_t);
+    size_t count;
 
-    for (short i = 0; i < currentAudioSampleCount; ++i)
-    {
-        int sample = stereoAudioBuffer[i] * volume_level;
-        if (sample > 32767)
-            sample = 32767;
-        else if (sample < -32767)
-            sample = -32767;
+    // Normalize the size of the sample to avoid size bigger than +- 32767
+    for(short i = 0; i < frameCount * 2; ++i){
+        int sample = stereoAudioBuffer[i] * volume_level; // Set the volumen level to the sample
+
+        if (sample > 32767) sample = 32767;
+        else if (sample < -32767) sample = -32767;
 
         stereoAudioBuffer[i] = (short)sample;
     }
 
-    int len = currentAudioSampleCount * sizeof(int16_t);
-    size_t count;
-    i2s_write(I2S_NUM, (const char *)stereoAudioBuffer, len, &count, portMAX_DELAY);
-    if (count != len)
-    {
-        printf("i2s_write_bytes: count (%d) != len (%d)\n", count, len);
+    i2s_write(I2S_NUM, (const char *)stereoAudioBuffer, audio_length, &count, portMAX_DELAY);
+
+    if(count != audio_length){
+        ESP_LOGE(TAG,"I2S Write error:\n Send count: %d\n Audio_Length: %d",count,audio_length);
         abort();
     }
 }
 
-void audio_terminate()
-{
-    i2s_zero_dma_buffer(I2S_NUM);
+void audio_terminate(){
+    i2s_zero_dma_buffer(I2S_NUM); // Clean the DMA buffer
     i2s_stop(I2S_NUM);
-
     i2s_start(I2S_NUM);
 }
 
 uint8_t audio_volume_get(){
+    ESP_LOGI(TAG,"Volumen level: %i",(uint8_t)volume_level*100);
     return volume_level*100;
 }
 
 void audio_volume_set(float level){
-    volume_level = level/100.0f;
-    printf("volume %f\r\n",volume_level);
+    if(level >= 0 || level <= 100) volume_level = level/100.0f;
+    ESP_LOGI(TAG,"Volumen level set: %i",(uint8_t)volume_level *100);
 }
--- components/drivers/sound/sound_driver.h
+++ components/drivers/sound/sound_driver.h
@@ -1,10 +1,83 @@
-#include "stdint.h"
+/*********************
+ *      DEFINES
+ *********************/
 
-#define I2S_NUM I2S_NUM_0
-#define VOLUME_LEVEL_COUNT (5)
+#define AUDIO_SAMPLE_8KHZ   8000
+#define AUDIO_SAMPLE_16KHZ  16000
+#define AUDIO_SAMPLE_32KHZ  32000
+#define AUDIO_SAMPLE_44kHZ  44100
 
-void audio_init(int sample_rate);
-void audio_submit(short *stereoAudioBuffer, int frameCount);
+/*********************
+ *      FUNCTIONS
+ *********************/
+
+/*
+ * Function:  audio_init 
+ * --------------------
+ * 
+ * Initialize the I2S peripherla with the configuration set on the file system_configuration.h.
+ * You can choose different audio sample rate:
+ *  - 8000 KHz
+ *  - 16000 KHz
+ *  - 32000 KHz
+ *  - 44100 KHz (Not recommend, it can produce overflow on the audio buffer)
+ * 
+ * Arguments:
+ *  -sample_rate: Audio sample rate to configure the driver.
+ * 
+ * Returns: True if the initialization succeed otherwise false.
+ * 
+ */
+bool audio_init(uint32_t sample_rate);
+
+/*
+ * Function:  audio_submit 
+ * --------------------
+ * 
+ * Proceess and send the audio samples to the I2S driver.
+ * 
+ * Arguments:
+ *  -stereoAudioBuffer: Pointer to the stereo audio buffer which previously should be filled by the emulator.
+ *  -framecount: To achieved sync audio, is necessary to receive the frame which is being display.
+ * 
+ * Returns: Nothing.
+ * 
+ */
+void audio_submit(short *stereoAudioBuffer, uint32_t frameCount);
+
+/*
+ * Function:  audio_terminate 
+ * --------------------
+ * 
+ * Clean the DMA audio buffer and reset the peripheral to avoid noise when the driver
+ * is not playing any sound.
+ * 
+ * Returns: Nothing.
+ * 
+ */
 void audio_terminate();
+
+/*
+ * Function:  audio_volume_get 
+ * --------------------
+ * 
+ * Give the volumen level.
+ * 
+ * Returns: 0-100 integer volumen level.
+ * 
+ */
 uint8_t audio_volume_get();
+
+/*
+ * Function:  audio_volume_set 
+ * --------------------
+ * 
+ * Set the volumen level.
+ * 
+ * Arguments:
+ *  -level: Float number from 0 to 1 in decimal increments.
+ * 
+ * Returns: Nothing.
+ * 
+ */
 void  audio_volume_set(float level);
