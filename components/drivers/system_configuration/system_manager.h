8dd1ff3647b09cc5985c42992dc87d355463d1eb
jfm92 <jfloresm92@gmail.com>
2020-11-24 22:08:03 +0100

    Added new modes and update the console list

--- components/drivers/system_configuration/system_manager.h
+++ components/drivers/system_configuration/system_manager.h
@@ -14,13 +14,16 @@ QueueHandle_t batteryQueue;
 #define MODE_UPDATE         0x05
 #define MODE_OUT            0x06
 #define MODE_BATTERY_ALERT  0x07
+#define MODE_CHANGE_VOLUME  0x08
+#define MODE_CHANGE_BRIGHT  0x09
 
 
-#define GAMEBOY 0x00
-#define GAMEBOY_COLOR 0x01
-#define NES 0x02
-#define SNES 0x03
-#define SMS 0x04
+#define GAMEBOY         0x00
+#define GAMEBOY_COLOR   0x01
+#define NES             0x02
+#define SNES            0x03
+#define SMS             0x04
+#define GG              0x005
 
 #define MEMORY_DMA 0x00
 #define MEMORY_INTERNAL 0x01
